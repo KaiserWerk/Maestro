0c60d52e72dce55bdd9a769da764db3868d3f031
Robin Kaiser <web@r-k.mx>
2021-11-25 22:24:19 +0100

    add logging package

---
+++ internal/logging/logging.go
@@ -0,0 +1,54 @@
+package logging
+
+import (
+	"io"
+	"os"
+
+	"github.com/KaiserWerk/Maestro/internal/shutdownManager"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Mode uint8
+
+const (
+	ModeConsole Mode = iota
+	ModeFile
+	ModeBoth
+)
+
+var (
+	err error
+	rotator *Rotator
+)
+
+func New(lvl logrus.Level, context string, mode Mode) *logrus.Entry {
+	l := logrus.New()
+	l.SetLevel(lvl)
+	l.SetFormatter(&MaestroFormatter{
+		LevelPadding: 7,
+		ContextPadding: 9,
+	})
+	l.SetReportCaller(false)
+	if mode == ModeConsole {
+		l.SetOutput(os.Stdout)
+	} else if mode == ModeFile {
+		l.SetOutput(rotator)
+	} else if mode == ModeBoth {
+		l.SetOutput(io.MultiWriter(rotator, os.Stdout))
+	}
+
+	return l.WithField("context", context)
+}
+
+func Init(dir string) {
+	shutdownManager.Register(CloseFileHandle)
+	rotator, err = NewRotator(dir, "maestro.log", 10 << 20, 0644)
+	if err != nil {
+		panic("cannot create rotator: " + err.Error())
+	}
+}
+
+func CloseFileHandle() error {
+	return rotator.Close()
+}
