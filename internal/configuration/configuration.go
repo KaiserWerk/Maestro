8c430cb11e3d4d41c7223818dc28ab962b1726a7
Robin Kaiser <web@r-k.mx>
2021-11-25 22:22:53 +0100

    add configuration package

---
+++ internal/configuration/configuration.go
@@ -0,0 +1,53 @@
+package configuration
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/KaiserWerk/Maestro/internal/assets"
+	"github.com/KaiserWerk/Maestro/internal/entity"
+
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	configFile = "app.yaml"
+)
+
+func SetFile(file string) {
+	configFile = file
+}
+
+func Setup() (*entity.AppConfig, bool, error) {
+	var created bool
+	if _, err := os.Stat(configFile); os.IsNotExist(err)  {
+		content, err := assets.ReadConfigurationFile("app.yaml")
+		if err != nil {
+			return nil, created, err
+		}
+
+		err = ioutil.WriteFile(configFile, content, 0744)
+		if err != nil {
+			return nil, created, err
+		}
+		created = true
+	}
+
+	conf, err := GetConfiguration()
+	return conf, created, err
+}
+
+func GetConfiguration() (*entity.AppConfig, error) {
+	content, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return nil, err
+	}
+
+	var conf entity.AppConfig
+
+	if err := yaml.Unmarshal(content, &conf); err != nil {
+		return nil, err
+	}
+
+	return &conf, nil
+}
