fb4282892fa7ea01de5459b6a7d456f6a73a2c05
Robin Kaiser <web@r-k.mx>
2022-02-07 12:00:14 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,11 +1,11 @@
 $sourcecode = ".\cmd\maestro\main.go"
 $target = "build\maestro"
-$version = "1.0.1"
+$version = "1.1.0"
 # Windows, 64-bit
 $env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-v$($version)-win-amd64.exe" -ldflags "-s -w" $sourcecode
 # Linux, 64-bit
 $env:GOOS = 'linux';   $env:GOARCH = 'amd64';             go build -o "$($target)-v$($version)-linux-amd64"   -ldflags "-s -w" $sourcecode
 # Raspberry Pi
-$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=6; go build -o "$($target)-v$($version)-linux-armv6"   -ldflags "-s -w" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=7; go build -o "$($target)-v$($version)-linux-armv7"   -ldflags "-s -w" $sourcecode
 # macOS
 $env:GOOS = 'darwin';  $env:GOARCH = 'amd64';             go build -o "$($target)-v$($version)-macos-amd64"   -ldflags "-s -w" $sourcecode
